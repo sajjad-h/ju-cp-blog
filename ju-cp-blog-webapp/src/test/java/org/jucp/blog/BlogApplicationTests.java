955790bbd08084625a553c55fd3403e40ea89f6a
sajjad-h <59442218+sajjad-h@users.noreply.github.com>
2022-06-07 01:42:26 +0600

    initial project setup

---
+++ ju-cp-blog-webapp/src/test/java/org/jucp/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jucp.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
